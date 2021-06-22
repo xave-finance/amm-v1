13f116d35d7fd71b3c1ae5da59a6f6866f05486b
Barry Lavides <lavidesbarry@gmail.com>
2021-06-22 19:39:34 +0800

    - Scripts for deploy and verify contracts - Scripts for creating new curve

---
+++ scripts/testnet/03-new-curve.ts
@@ -0,0 +1,162 @@
+require("dotenv").config();
+import { ethers } from "hardhat";
+
+import { TOKENS } from "../../test/Constants";
+import { Curve } from "../../typechain/Curve";
+import { ERC20 } from "../../typechain/ERC20";
+import { BigNumberish, Signer } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+
+const NAME = "DFX V1";
+const SYMBOL = "DFX-V1";
+
+// Weights are always 50/50
+
+// Pool must respect a 10/90 ratio
+// i.e. value of one pair cannot exceed 90% of the pools value
+const ALPHA = parseUnits("0.8");
+
+// Slippage (fees) will that will be introduced when one of the tokens's ratio:
+// - exceeds 75% of the pool value
+// - goes under 25% of the pool value
+const BETA = parseUnits("0.5");
+
+const MAX = parseUnits("0.15");
+const EPSILON = parseUnits("0.0005"); // 5 basis point
+const LAMBDA = parseUnits("0.3");
+
+export const getDeployer = async (): Promise<{
+  deployer: Signer;
+}> => {
+  const [deployer] = await ethers.getSigners();
+  return {
+    deployer
+  };
+};
+
+const CONTRACT_CURVE_FACTORY_ADDR = process.env.CONTRACT_CURVE_FACTORY_ADDR;
+const CONTRACT_CADCTOUSDASSIMILATOR_ADDR = process.env.CONTRACT_CADCTOUSDASSIMILATOR_ADDR;
+const CONTRACT_USDCTOUSDASSIMILATOR_ADDR = process.env.CONTRACT_USDCTOUSDASSIMILATOR_ADDR;
+
+async function main() {
+  const { deployer } = await getDeployer();
+
+  console.log(`Setting up scaffolding at network ${ethers.provider.connection.url}`);
+  console.log(`Deployer account: ${await deployer.getAddress()}`);
+
+  const curveFactory = (await ethers.getContractAt("CurveFactory", CONTRACT_CURVE_FACTORY_ADDR)) as Curve;
+  const usdc = (await ethers.getContractAt("ERC20", TOKENS.USDC.address)) as ERC20;
+  const cadc = (await ethers.getContractAt("ERC20", TOKENS.CADC.address)) as ERC20;
+
+  const createCurve = async function ({
+    name,
+    symbol,
+    base,
+    quote,
+    baseWeight,
+    quoteWeight,
+    baseAssimilator,
+    quoteAssimilator,
+  }: {
+    name: string;
+    symbol: string;
+    base: string;
+    quote: string;
+    baseWeight: BigNumberish;
+    quoteWeight: BigNumberish;
+    baseAssimilator: string;
+    quoteAssimilator: string;
+  }): Promise<{ curve: Curve; curveLpToken: ERC20 }> {
+    const tx = await curveFactory.newCurve(
+      name,
+      symbol,
+      base,
+      quote,
+      baseWeight,
+      quoteWeight,
+      baseAssimilator,
+      quoteAssimilator,
+      {
+        gasLimit: 12000000,
+      },
+    );
+    await tx.wait();
+
+    console.log('CurveFactory#newCurve TX Hash: ', tx.hash)
+
+    // Get curve address
+    const curveAddress = await curveFactory.curves(
+      ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["address", "address"], [base, quote])),
+    );
+    console.log('CurveFactory#curves Address: ', curveAddress)
+    const curveLpToken = (await ethers.getContractAt("ERC20", curveAddress)) as ERC20;
+    const curve = (await ethers.getContractAt("Curve", curveAddress)) as Curve;
+
+    const turnOffWhitelisting = await curve.turnOffWhitelisting();
+    console.log('Curve#turnOffWhitelisting TX Hash: ', turnOffWhitelisting.hash)
+
+    return {
+      curve,
+      curveLpToken,
+    };
+  };
+
+  const createCurveAndSetParams = async function ({
+    name,
+    symbol,
+    base,
+    quote,
+    baseWeight,
+    quoteWeight,
+    baseAssimilator,
+    quoteAssimilator,
+    params,
+  }: {
+    name: string;
+    symbol: string;
+    base: string;
+    quote: string;
+    baseWeight: BigNumberish;
+    quoteWeight: BigNumberish;
+    baseAssimilator: string;
+    quoteAssimilator: string;
+    params: [BigNumberish, BigNumberish, BigNumberish, BigNumberish, BigNumberish];
+  }) {
+    const { curve, curveLpToken } = await createCurve({
+      name,
+      symbol,
+      base,
+      quote,
+      baseWeight,
+      quoteWeight,
+      baseAssimilator,
+      quoteAssimilator,
+    });
+
+    return {
+      curve,
+      curveLpToken,
+    };
+  };
+
+  const { curve: curveCADC } = await createCurveAndSetParams({
+    name: NAME,
+    symbol: SYMBOL,
+    base: cadc.address,
+    quote: usdc.address,
+    baseWeight: parseUnits("0.5"),
+    quoteWeight: parseUnits("0.5"),
+    baseAssimilator: CONTRACT_CADCTOUSDASSIMILATOR_ADDR,
+    quoteAssimilator: CONTRACT_USDCTOUSDASSIMILATOR_ADDR,
+    params: [ALPHA, BETA, MAX, EPSILON, LAMBDA],
+  });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
