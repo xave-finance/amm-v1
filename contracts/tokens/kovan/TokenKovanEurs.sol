fbbfa4d118a5e2389cfbe6081a7466b59153709b
Barry Lavides <lavidesbarry@gmail.com>
2021-07-15 08:54:08 +0800

    kovan token contracts and script

---
+++ contracts/tokens/kovan/TokenKovanEurs.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenKovanEurs is ERC20 {
+    string public functionCalled;
+    uint8 public constant DECIMALS = 8;
+    uint256 public constant INITIAL_SUPPLY = 1000000 * (10**uint256(DECIMALS));
+
+    constructor() public ERC20("EURS", "EURS") {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    fallback() external payable {
+        functionCalled = "fallback";
+    }
+
+    function ethBal() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
