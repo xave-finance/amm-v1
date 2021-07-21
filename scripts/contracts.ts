dc6ce6a5ca6b53a22e34df65743ba92606864674
Barry Lavides <lavidesbarry@gmail.com>
2021-07-21 20:46:03 +0800

    - added scripts/factory_deployed.json in .gitignore - added deployed_contract_logs in git - contracts.ts for contract addresses import from json files

---
+++ scripts/contracts.ts
@@ -0,0 +1,16 @@
+// import { CONTRACTS } from "./contracts";
+
+import * as factory from "./factory_deployed.json";
+// import * as assimilators from "./assimilators_deployed.json";
+
+export const CONTRACTS = {
+  factory: factory.curveFactory,
+  curves: factory.libraries.Curves,
+  orchestrator: factory.libraries.Orchestrator,
+  swaps: factory.libraries.Swaps,
+  viewLiquidity: factory.libraries.ViewLiquidity,
+  // cadcToUsdAssimilator: assimilators.cadcToUsdAssimilator,
+  // usdcToUsdAssimilator: assimilators.usdcToUsdAssimilator,
+  // eursToUsdAssimilator: assimilators.eursToUsdAssimilator,
+  // xsgdToUsdAssimilator: assimilators.xsgdToUsdAssimilator,
+};
