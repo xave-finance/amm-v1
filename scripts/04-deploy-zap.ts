ccb203bca72a6195198cacf58b7d87e950d3ae94
Barry Lavides <lavidesbarry@gmail.com>
2021-07-22 13:52:24 +0800

    kovan contract deployed logs for zap

--- scripts/04-deploy-zap.ts
+++ scripts/04-deploy-zap.ts
@@ -2,6 +2,7 @@ import hre from "hardhat";
 import chalk from "chalk";
 import path from "path";
 import fs from "fs";
+import mkdirp from "mkdirp";
 
 import { getAccounts, deployContract } from "./common";
 
@@ -29,9 +30,15 @@ async function main() {
     zap: zap.address,
   };
 
+  // Deployed contracts log
+  const outputLogDir = path.join(__dirname, `./deployed_contract_logs/${hre.network.name}`);
+  // Just like mkdir -p, it will create directory if it doesn't exist
+  // If it already exists, then it will not print an error and will move further to create sub-directories.
+  mkdirp.sync(outputLogDir);
+
   const timestamp = new Date().getTime().toString();
-  const outputPath = path.join(__dirname, `./deployed_contract_logs/${timestamp}_zap_deployed.json`);
-  fs.writeFileSync(outputPath, JSON.stringify(output, null, 4));
+  const outputLogPath = path.join(`${outputLogDir}/${timestamp}_zap_deployed.json`);
+  fs.writeFileSync(outputLogPath, JSON.stringify(output, null, 4));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
