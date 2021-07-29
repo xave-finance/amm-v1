892037c7c11aa7db6f80febe11d8c1c44891306e
Barry Lavides <lavidesbarry@gmail.com>
2021-07-29 15:40:10 +0800

    add liquidity formatting precision

--- scripts/test/add-liquidity.ts
+++ scripts/test/add-liquidity.ts
@@ -1,82 +1,71 @@
-import hre from "hardhat";
-import chalk from "chalk";
 import { ethers } from "hardhat";
-import { mintEURS, mintUSDC, getFutureTime } from "../../test/Utils";
+import { parseUnits, formatUnits } from "ethers/lib/utils";
+
+import { getFutureTime } from "../../test/Utils";
 import { Curve } from "../../typechain/Curve";
 import { ERC20 } from "../../typechain/ERC20";
-import { BigNumberish, Signer } from "ethers";
-import { parseUnits, parseEther, formatEther, formatUnits } from "ethers/lib/utils";
-import { getAccounts, getFastGasPrice } from "../common";
-import { CONTRACTS } from "../config/contracts";
+import { getAccounts } from "../common";
+import { curveImporter } from "../Utils";
 
-const CORE_ADDRESSES = {
-  curveFactory: CONTRACTS.factory
-}
+const eursCurveAddr = require(curveImporter('curve_EURS_deployed')).curveAddress;
 
-const CONTRACT_CURVE_EURS_ADDR = "0x50d70784BBc9363408914Ab26d4897d469DD3DA1";
+const CONTRACT_CURVE_EURS_ADDR = eursCurveAddr;
 const TOKEN_USDC = process.env.TOKEN_USDC;
 const TOKEN_EURS = process.env.TOKEN_EURS;
-const TOKENS_USDC_DECIMALS = 6;
-const TOKENS_EURS_DECIMALS = 2;
+const TOKENS_USDC_DECIMALS = process.env.TOKENS_USDC_DECIMALS;
+const TOKENS_EURS_DECIMALS = process.env.TOKENS_EURS_DECIMALS;
 
 async function main() {
   const { user1 } = await getAccounts();
   const erc20 = (await ethers.getContractAt("ERC20", ethers.constants.AddressZero)) as ERC20;
 
-  const mintAndApprove = async function (
-    tokenAddress: string,
-    minter: Signer,
-    amount: BigNumberish,
-    recipient: string,
-  ) {
-    await erc20.attach(tokenAddress).connect(minter).approve(recipient, amount);
-  };
+  // Approve tokens
+  await erc20.attach(TOKEN_USDC).connect(user1)
+    .approve(CONTRACT_CURVE_EURS_ADDR, parseUnits("10000000", TOKENS_USDC_DECIMALS));
+  await erc20.attach(TOKEN_EURS).connect(user1)
+    .approve(CONTRACT_CURVE_EURS_ADDR, parseUnits("10000000", TOKENS_EURS_DECIMALS));
 
-  const multiMintAndApprove = async function (requests: [string, Signer, BigNumberish, string][]) {
-    for (let i = 0; i < requests.length; i++) {
-      await mintAndApprove(...requests[i]);
-    }
-  };
-
-  // Mint tokens and approve
-  await multiMintAndApprove([
-    [TOKEN_USDC, user1, parseUnits("10000000", TOKENS_USDC_DECIMALS), CONTRACT_CURVE_EURS_ADDR],
-    [TOKEN_EURS, user1, parseUnits("10000000", TOKENS_EURS_DECIMALS), CONTRACT_CURVE_EURS_ADDR]
-  ]);
-
+  const amt = parseUnits("5");
   const curveEURS = (await ethers.getContractAt("Curve", CONTRACT_CURVE_EURS_ADDR)) as Curve;
+  const eurs = (await ethers.getContractAt("ERC20", TOKEN_EURS)) as ERC20;
+  const usdc = (await ethers.getContractAt("ERC20", TOKEN_USDC)) as ERC20;
 
-  const amt = parseUnits("1000000");
-  const viewDepositCurveEURS = await curveEURS.connect(user1).viewDeposit(amt);
+  const eursBalBefore = formatUnits(await eurs.balanceOf(user1.address), TOKENS_EURS_DECIMALS);
+  const usdcBalBefore = formatUnits(await usdc.balanceOf(user1.address), TOKENS_USDC_DECIMALS);
+  const eursAllowanceBefore = formatUnits(await eurs.allowance(user1.address, CONTRACT_CURVE_EURS_ADDR), TOKENS_EURS_DECIMALS);
+  const usdcAllowanceBefore = formatUnits(await usdc.allowance(user1.address, CONTRACT_CURVE_EURS_ADDR), TOKENS_USDC_DECIMALS);
+
+  console.log('BASE Amout to Deposit: ', formatUnits(amt));
+  console.log('EURS Bal Before: ', eursBalBefore);
+  console.log('USDC Bal Before: ', usdcBalBefore);
+  console.log('EURS Allowance Before: ', eursAllowanceBefore);
+  console.log('USDC Allowance Before: ', usdcAllowanceBefore);
+
+  const [lpAmountUser1, [baseViewUser1, quoteViewUser1]] = await curveEURS.viewDeposit(amt);
 
   console.log('-----------------------------------------------------------------------');
   console.log('Liquidity To Deposit');
   console.log('-----------------------------------------------------------------------');
-  console.log('INPUT: ', amt);
-  console.log('EURS AMT: ', formatUnits(viewDepositCurveEURS[1][0], TOKENS_EURS_DECIMALS));
-  console.log('USDC AMT: ', formatUnits(viewDepositCurveEURS[1][1], TOKENS_USDC_DECIMALS));
+  console.log('EURS AMT: ', formatUnits(baseViewUser1, TOKENS_EURS_DECIMALS));
+  console.log('USDC AMT: ', formatUnits(quoteViewUser1, TOKENS_USDC_DECIMALS));
 
   // Supply liquidity to the pools
   const depositCurveEURS = await curveEURS
-    .connect(user1)
-    .deposit(amt, await getFutureTime(), {
-      gasLimit: 12000000,
-    })
+    .deposit(amt, await getFutureTime(), { gasLimit: 12000000 })
     .then(x => x.wait());
 
-  // Check pool liquidity
-  const viewLiquidity = await curveEURS
-    .connect(user1)
-    .liquidity();
+  console.log('depositCurveEURS', depositCurveEURS);
 
+  // Check pool liquidity
+  const [lpAmount, [baseBal, quoteBal]] = await curveEURS
+    .liquidity();
   console.log('-----------------------------------------------------------------------');
   console.log('Liquidity Balance');
   console.log('-----------------------------------------------------------------------');
-  console.log('Total: ', formatUnits(viewLiquidity.total_));
-  console.log('EURS: ', formatUnits(viewLiquidity.individual_[0], TOKENS_EURS_DECIMALS));
-  console.log('USDC: ', formatUnits(viewLiquidity.individual_[1], TOKENS_USDC_DECIMALS))
 
-  console.log(`User1 balance: ${await user1.getBalance()}`);
+  console.log('Total: ', formatUnits(lpAmount));
+  console.log('EURS: ', formatUnits(baseBal, TOKENS_EURS_DECIMALS));
+  console.log('USDC: ', formatUnits(quoteBal, TOKENS_USDC_DECIMALS));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
