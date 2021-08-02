2d32b01c76d468448c27c6e31861eb9e6b730768
Barry Lavides <lavidesbarry@gmail.com>
2021-08-02 18:00:49 +0800

    added turn off whitelisting script

--- scripts/test/turnoffwhitelist.ts
+++ scripts/test/turnoffwhitelist.ts
@@ -1,31 +1,32 @@
-import hre from "hardhat";
-import chalk from "chalk";
 import { ethers } from "hardhat";
+import { parseUnits, formatUnits } from "ethers/lib/utils";
+import { BigNumberish, Signer } from "ethers";
+import hre from "hardhat";
+
 import { mintEURS, mintUSDC, getFutureTime } from "../../test/Utils";
 import { Curve } from "../../typechain/Curve";
 import { ERC20 } from "../../typechain/ERC20";
-import { BigNumberish, Signer } from "ethers";
-import { parseUnits, parseEther, formatEther, formatUnits } from "ethers/lib/utils";
-import { getAccounts, getFastGasPrice } from "../common";
+import { getAccounts } from "../common";
+import { configImporter } from "../Utils";
 
-const CONTRACT_CURVE_EURS_ADDR = "0x6e1a1B3bf4c01EB602F4872711bcCE4cc4b63AE9";
-const TOKEN_USDC = process.env.TOKEN_USDC;
-const TOKEN_EURS = process.env.TOKEN_EURS;
-const TOKENS_USDC_DECIMALS = 6;
-const TOKENS_EURS_DECIMALS = 2;
+const eursCurveAddr = require(configImporter('curve_EURS_deployed')).curveAddress;
+
+const CONTRACT_CURVE_EURS_ADDR = eursCurveAddr;
+
 
 async function main() {
-  const { user2 } = await getAccounts();
+  console.time('Deployment Time');
 
   const curveEURS = (await ethers.getContractAt("Curve", CONTRACT_CURVE_EURS_ADDR)) as Curve;
 
-  // Supply liquidity to the pools
-  const depositCurveEURS = await curveEURS
-    .connect(user2)
-    .turnOffWhitelisting()
-    .then(x => x.wait());
-
-  console.log(`User2 balance: ${await user2.getBalance()}`);
+  try {
+    const turnOffWhitelisting = await curveEURS.turnOffWhitelisting();
+    console.log('Curve#turnOffWhitelisting TX Hash: ', turnOffWhitelisting.hash)
+    console.timeEnd('Deployment Time');
+  } catch (error) {
+    console.log(error);
+    console.timeEnd('Deployment Time');
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
