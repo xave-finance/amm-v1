aa3adebc55cb8b4b4135f2a3b7e863e0d1e92cfc
Barry Lavides <lavidesbarry@gmail.com>
2021-10-07 22:33:19 +0800

    deposit tests for eurs and cadc

--- test/03-Liquidity.ts
+++ test/03-Liquidity.ts
@@ -186,7 +186,7 @@ describe("Curve Contract", () => {
             "base",
             await previewDepositGivenBase(deposit, rateBase, tokenQuote, weightBase, curve),
             deposit,
-            TOKENS.XSGD.decimals,
+            TOKENS[tokenQuote].decimals,
             curve
           );
 
@@ -211,7 +211,7 @@ describe("Curve Contract", () => {
       }
     });
 
-    describe("deposit when pool have unbalanced ratio", () => {
+    describe("pool deposit with unbalanced ratio", () => {
       for (let swapAmt = 1; swapAmt < maxSwap; swapAmt *= 10) {
         const amt = parseUnits(swapAmt.toString(), TOKENS[tokenQuote].decimals);
 
@@ -238,6 +238,296 @@ describe("Curve Contract", () => {
 
           // Preview given base
           const rateBase = Number(formatUnits(await xsgdToUsdAssimilator.getRate(), 8));
+          // Estimate deposit given base
+          const depositPreviewGivenBase = await adjustViewDeposit(
+            "base",
+            await previewDepositGivenBase(swapAmt, rateBase, tokenQuote, weightBase, curve),
+            swapAmt,
+            TOKENS[tokenQuote].decimals,
+            curve
+          );
+
+          console.log("Estimate Base: ", formatUnits(depositPreviewGivenBase.base, TOKENS[tokenQuote].decimals));
+          // User input should be gte the estimate base
+          expect(parseUnits(swapAmt.toString()).gte(depositPreviewGivenBase.base))
+        });
+      }
+    });
+  });
+
+  describe("viewDeposit on EURS/USDC curve", () => {
+    const tokenQuote = "EURS";
+
+    before(async () => {
+      const NAME = `Token ${tokenQuote}`;
+      const SYMBOL = tokenQuote;
+      baseToken = (await ethers.getContractAt("ERC20", TOKENS[tokenQuote].address)) as ERC20;
+      quoteToken = (await ethers.getContractAt("ERC20", TOKENS.USDC.address)) as ERC20;
+
+      ({ curve } = await createCurveAndSetParams({
+        name: NAME,
+        symbol: SYMBOL,
+        base: TOKENS[tokenQuote].address,
+        quote: TOKENS.USDC.address,
+        baseWeight: parseUnits(".5"),
+        quoteWeight: parseUnits(".5"),
+        baseAssimilator: eursToUsdAssimilator.address,
+        quoteAssimilator: usdcToUsdAssimilator.address,
+        params: [DIMENSION.alpha, DIMENSION.beta, DIMENSION.max, DIMENSION.epsilon, DIMENSION.lambda],
+      }));
+
+      // Approve Deposit
+      await multiMintAndApprove([
+        [TOKENS[tokenQuote].address, user1, parseUnits("900000000000", TOKENS[tokenQuote].decimals), curve.address],
+        [TOKENS.USDC.address, user1, parseUnits("900000000000", TOKENS.USDC.decimals), curve.address],
+      ]);
+    });
+
+    describe("given quote as input", () => {
+      for (let deposit = 1; deposit <= maxDeposit; deposit *= 10) {
+        it(`it returns estimated quote similar to input quote: ${deposit}`, async () => {
+          // Estimate deposit given quote
+          const depositPreview = await adjustViewDeposit(
+            "quote",
+            await previewDepositGivenQuote(deposit, tokenQuote, curve),
+            deposit,
+            TOKENS.USDC.decimals,
+            curve
+          );
+
+          console.log("Estimate Quote: ", formatUnits(depositPreview.quote, TOKENS.USDC.decimals));
+
+          // User input should be gte the estimate quote
+          expect(parseUnits(deposit.toString()).gte(depositPreview.quote))
+
+          const baseBalA: BigNumber = await baseToken.balanceOf(user1Address);
+          const quoteBalA: BigNumber = await quoteToken.balanceOf(user1Address);
+
+          // Deposit
+          await curve.deposit(parseUnits(depositPreview.deposit.toString()), await getFutureTime());
+
+          const baseBalB: BigNumber = await baseToken.balanceOf(user1Address);
+          const quoteBalB: BigNumber = await quoteToken.balanceOf(user1Address);
+
+          // Compare balance before and after deposit
+          expect(baseBalA.sub(depositPreview.base).gte(baseBalB)).to.be.true;
+          expect(quoteBalA.sub(depositPreview.quote).gte(quoteBalB)).to.be.true;
+        });
+      }
+    });
+
+    describe("given base as input", () => {
+      for (let deposit = 1; deposit <= maxDeposit; deposit *= 10) {
+        it(`it returns estimated base similar to input base: ${deposit}`, async () => {
+          // Preview given base
+          const rateBase = Number(formatUnits(await eursToUsdAssimilator.getRate(), 8));
+          const liquidity = await curve.liquidity();
+          const liquidityTotal = parseFloat(formatUnits(liquidity.total_));
+          const numeraireBase = parseFloat(formatUnits(liquidity.individual_[0]));
+          const weightBase = numeraireBase / liquidityTotal;
+
+          // Estimate deposit given base
+          const depositPreview = await adjustViewDeposit(
+            "base",
+            await previewDepositGivenBase(deposit, rateBase, tokenQuote, weightBase, curve),
+            deposit,
+            TOKENS[tokenQuote].decimals,
+            curve
+          );
+
+          console.log("Estimate Base: ", formatUnits(depositPreview.base, TOKENS[tokenQuote].decimals));
+
+          // User input should be gte the estimate base
+          expect(parseUnits(deposit.toString()).gte(depositPreview.base))
+
+          const baseBalA: BigNumber = await baseToken.balanceOf(user1Address);
+          const quoteBalA: BigNumber = await quoteToken.balanceOf(user1Address);
+
+          // Deposit
+          await curve.deposit(parseUnits(depositPreview.deposit.toString()), await getFutureTime());
+
+          const baseBalB: BigNumber = await baseToken.balanceOf(user1Address);
+          const quoteBalB: BigNumber = await quoteToken.balanceOf(user1Address);
+
+          // Compare balance before and after deposit
+          expect(baseBalA.sub(depositPreview.base).gte(baseBalB)).to.be.true;
+          expect(quoteBalA.sub(depositPreview.quote).gte(quoteBalB)).to.be.true;
+        });
+      }
+    });
+
+    describe("pool deposit with unbalanced ratio", () => {
+      for (let swapAmt = 1; swapAmt < maxSwap; swapAmt *= 10) {
+        const amt = parseUnits(swapAmt.toString(), TOKENS[tokenQuote].decimals);
+
+        it(`swapping amount: ${amt}`, async () => {
+          await curve
+            .originSwap(baseToken.address, TOKENS.USDC.address, amt, 0, await getFutureTime());
+          const liquidity = await curve.liquidity();
+          const liquidityTotal = parseFloat(formatUnits(liquidity.total_));
+          const numeraireBase = parseFloat(formatUnits(liquidity.individual_[0]));
+          const weightBase = numeraireBase / liquidityTotal;
+
+          // Estimate deposit given quote
+          const depositPreviewGivenQuote = await adjustViewDeposit(
+            "quote",
+            await previewDepositGivenQuote(swapAmt, tokenQuote, curve),
+            swapAmt,
+            TOKENS.USDC.decimals,
+            curve
+          );
+
+          console.log("Estimate Quote: ", formatUnits(depositPreviewGivenQuote.quote, TOKENS.USDC.decimals));
+          // User input should be gte the estimate quote
+          expect(parseUnits(swapAmt.toString()).gte(depositPreviewGivenQuote.quote))
+
+          // Preview given base
+          const rateBase = Number(formatUnits(await eursToUsdAssimilator.getRate(), 8));
+          // Estimate deposit given base
+          const depositPreviewGivenBase = await adjustViewDeposit(
+            "base",
+            await previewDepositGivenBase(swapAmt, rateBase, tokenQuote, weightBase, curve),
+            swapAmt,
+            TOKENS[tokenQuote].decimals,
+            curve
+          );
+
+          console.log("Estimate Base: ", formatUnits(depositPreviewGivenBase.base, TOKENS[tokenQuote].decimals));
+          // User input should be gte the estimate base
+          expect(parseUnits(swapAmt.toString()).gte(depositPreviewGivenBase.base))
+        });
+      }
+    });
+  });
+
+  describe("viewDeposit on CADC/USDC curve", () => {
+    const tokenQuote = "CADC";
+
+    before(async () => {
+      const NAME = `Token ${tokenQuote}`;
+      const SYMBOL = tokenQuote;
+      baseToken = (await ethers.getContractAt("ERC20", TOKENS[tokenQuote].address)) as ERC20;
+      quoteToken = (await ethers.getContractAt("ERC20", TOKENS.USDC.address)) as ERC20;
+
+      ({ curve } = await createCurveAndSetParams({
+        name: NAME,
+        symbol: SYMBOL,
+        base: TOKENS[tokenQuote].address,
+        quote: TOKENS.USDC.address,
+        baseWeight: parseUnits(".5"),
+        quoteWeight: parseUnits(".5"),
+        baseAssimilator: cadcToUsdAssimilator.address,
+        quoteAssimilator: usdcToUsdAssimilator.address,
+        params: [DIMENSION.alpha, DIMENSION.beta, DIMENSION.max, DIMENSION.epsilon, DIMENSION.lambda],
+      }));
+
+      // Approve Deposit
+      await multiMintAndApprove([
+        [TOKENS[tokenQuote].address, user1, parseUnits("900000000000", TOKENS[tokenQuote].decimals), curve.address],
+        [TOKENS.USDC.address, user1, parseUnits("900000000000", TOKENS.USDC.decimals), curve.address],
+      ]);
+    });
+
+    describe("given quote as input", () => {
+      for (let deposit = 1; deposit <= maxDeposit; deposit *= 10) {
+        it(`it returns estimated quote similar to input quote: ${deposit}`, async () => {
+          // Estimate deposit given quote
+          const depositPreview = await adjustViewDeposit(
+            "quote",
+            await previewDepositGivenQuote(deposit, tokenQuote, curve),
+            deposit,
+            TOKENS.USDC.decimals,
+            curve
+          );
+
+          console.log("Estimate Quote: ", formatUnits(depositPreview.quote, TOKENS.USDC.decimals));
+
+          // User input should be gte the estimate quote
+          expect(parseUnits(deposit.toString()).gte(depositPreview.quote))
+
+          const baseBalA: BigNumber = await baseToken.balanceOf(user1Address);
+          const quoteBalA: BigNumber = await quoteToken.balanceOf(user1Address);
+
+          // Deposit
+          await curve.deposit(parseUnits(depositPreview.deposit.toString()), await getFutureTime());
+
+          const baseBalB: BigNumber = await baseToken.balanceOf(user1Address);
+          const quoteBalB: BigNumber = await quoteToken.balanceOf(user1Address);
+
+          // Compare balance before and after deposit
+          expect(baseBalA.sub(depositPreview.base).gte(baseBalB)).to.be.true;
+          expect(quoteBalA.sub(depositPreview.quote).gte(quoteBalB)).to.be.true;
+        });
+      }
+    });
+
+    describe("given base as input", () => {
+      for (let deposit = 1; deposit <= maxDeposit; deposit *= 10) {
+        it(`it returns estimated base similar to input base: ${deposit}`, async () => {
+          // Preview given base
+          const rateBase = Number(formatUnits(await cadcToUsdAssimilator.getRate(), 8));
+          const liquidity = await curve.liquidity();
+          const liquidityTotal = parseFloat(formatUnits(liquidity.total_));
+          const numeraireBase = parseFloat(formatUnits(liquidity.individual_[0]));
+          const weightBase = numeraireBase / liquidityTotal;
+
+          // Estimate deposit given base
+          const depositPreview = await adjustViewDeposit(
+            "base",
+            await previewDepositGivenBase(deposit, rateBase, tokenQuote, weightBase, curve),
+            deposit,
+            TOKENS[tokenQuote].decimals,
+            curve
+          );
+
+          console.log("Estimate Base: ", formatUnits(depositPreview.base, TOKENS[tokenQuote].decimals));
+
+          // User input should be gte the estimate base
+          expect(parseUnits(deposit.toString()).gte(depositPreview.base))
+
+          const baseBalA: BigNumber = await baseToken.balanceOf(user1Address);
+          const quoteBalA: BigNumber = await quoteToken.balanceOf(user1Address);
+
+          // Deposit
+          await curve.deposit(parseUnits(depositPreview.deposit.toString()), await getFutureTime());
+
+          const baseBalB: BigNumber = await baseToken.balanceOf(user1Address);
+          const quoteBalB: BigNumber = await quoteToken.balanceOf(user1Address);
+
+          // Compare balance before and after deposit
+          expect(baseBalA.sub(depositPreview.base).gte(baseBalB)).to.be.true;
+          expect(quoteBalA.sub(depositPreview.quote).gte(quoteBalB)).to.be.true;
+        });
+      }
+    });
+
+    describe("pool deposit with unbalanced ratio", () => {
+      for (let swapAmt = 1; swapAmt < maxSwap; swapAmt *= 10) {
+        const amt = parseUnits(swapAmt.toString(), TOKENS[tokenQuote].decimals);
+
+        it(`swapping amount: ${amt}`, async () => {
+          await curve
+            .originSwap(baseToken.address, TOKENS.USDC.address, amt, 0, await getFutureTime());
+          const liquidity = await curve.liquidity();
+          const liquidityTotal = parseFloat(formatUnits(liquidity.total_));
+          const numeraireBase = parseFloat(formatUnits(liquidity.individual_[0]));
+          const weightBase = numeraireBase / liquidityTotal;
+
+          // Estimate deposit given quote
+          const depositPreviewGivenQuote = await adjustViewDeposit(
+            "quote",
+            await previewDepositGivenQuote(swapAmt, tokenQuote, curve),
+            swapAmt,
+            TOKENS.USDC.decimals,
+            curve
+          );
+
+          console.log("Estimate Quote: ", formatUnits(depositPreviewGivenQuote.quote, TOKENS.USDC.decimals));
+          // User input should be gte the estimate quote
+          expect(parseUnits(swapAmt.toString()).gte(depositPreviewGivenQuote.quote))
+
+          // Preview given base
+          const rateBase = Number(formatUnits(await cadcToUsdAssimilator.getRate(), 8));
           // Estimate deposit given base
           const depositPreviewGivenBase = await adjustViewDeposit(
             "base",
