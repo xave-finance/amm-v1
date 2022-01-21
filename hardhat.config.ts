f03a46ca312d2168097dea4b16e4bb11346e473f
Geonell Montehermoso <geonell@oostvoort.com>
2022-01-21 14:45:31 +0800

    add semicolon

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,9 +8,9 @@ const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
 const ALCHEMY_PROJECT_ID = process.env.ALCHEMY_PROJECT_ID;
 const alchemyEndpoint = `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_PROJECT_ID}`;
 const MNEMONIC = process.env.MNEMONIC;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
-const POLYSCAN_API_KEY = process.env.POLYSCAN_API_KEY || ''
-const ARBISCAN_API_KEY = process.env.ARBISCAN_API_KEY || ''
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+const POLYSCAN_API_KEY = process.env.POLYSCAN_API_KEY || '';
+const ARBISCAN_API_KEY = process.env.ARBISCAN_API_KEY || '';
 import { CONFIG } from "./test/Config";
 const LOCALHOST = "http://127.0.0.1:8545";
 
